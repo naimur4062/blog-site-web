7ce4eef44ef2f68b2b00c9623a46fa1e3109b603
naimur4062 <naimur4062@gmail.com>
2021-07-06 10:54:20 +0800

    update

--- src/components/User/Reply/Reply.js
+++ src/components/User/Reply/Reply.js
@@ -1,19 +1,31 @@
 import React from 'react';
 
 const Reply = ({ replyDetails }) => {
-    const { replierName, replierPhoto, reply, date } = replyDetails;
+    const { replierName, replierPhoto, reply, date, _id } = replyDetails;
+
+    const handleReplyDelete = (id) => {
+        fetch(`http://localhost:5000/deleteReply/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => {
+                if (res) {
+                    console.log('delete successful')
+                };
+            });
+    };
+
     return (
         <div>
             {/* <p>all custom css are applying in showingComment.css file</p> */}
             <div className="reply-showing d-flex ml-3 mt-3">
                 <img src={replierPhoto} alt="" />
-                <div className="comment-details">
+                <div className="reply-details">
                     <p><span className="name">{replierName}</span> <br /> {reply}</p>
                 </div>
             </div>
             <div className="d-flex reply-function">
                 <p>{new Date(date).toLocaleString().split(',')[0]}</p>
-                <p className="delete">Delete</p>
+                <p onClick={() => handleReplyDelete(_id)} className="delete">Delete</p>
             </div>
         </div>
     );
